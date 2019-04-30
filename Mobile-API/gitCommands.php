c344e41c7afa08e4efed2b6d6ade548e62054d00
Arvind Rawat <ar9786@gmail.com>
2019-04-30 16:02:54 +0530

    Second Save

---
+++ Mobile-API/gitCommands.php
@@ -0,0 +1,10 @@
+git init
+git add *
+git commit -m "First Save"
+git remote add origin https://github.com/ar9786/verkoop.git-->your url
+git push origin master || 
+git status
+
+
+for cloning
+git clone ar9786@host:https://github.com/ar9786/Curl---
